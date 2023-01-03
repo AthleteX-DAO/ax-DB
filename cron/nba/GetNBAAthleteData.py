b58aa44f6733eecf3b3e78c53219eaee193e4c6b
Alex <blackwellag@appstate.edu>
2023-01-03 11:56:47 -0500

    enc[aes256_gmc done (hopefully)

---
+++ cron/nba/GetNBAAthleteData.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import requests                  
+
+# Constants
+API_URL = 'http://ax-api-athletex-api:8080/nba/players/update'
+
+def syncLatestStats():
+        httpResponse = requests.get(API_URL)
+
+try:
+  syncLatestStats()
+except:
+  print("Error occurred ")
